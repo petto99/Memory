f068a919e92a371a97f07cfc62efaff0ff105171
JiHyeon Kim <petto99@naver.com>
2016-09-06 20:06:35 +0900

    finish

--- Application/build/generated/source/r/debug/com/example/android/navigationdrawer/R.java
+++ Application/build/generated/source/r/debug/com/example/android/navigationdrawer/R.java
@@ -1927,17 +1927,31 @@ containing a value of this type.
         public static final int action_search=0x7f02004c;
         public static final int drawer_shadow=0x7f02004d;
         public static final int earth=0x7f02004e;
-        public static final int ic_drawer=0x7f02004f;
-        public static final int ic_launcher=0x7f020050;
-        public static final int jupiter=0x7f020051;
-        public static final int mars=0x7f020052;
-        public static final int mercury=0x7f020053;
-        public static final int neptune=0x7f020054;
-        public static final int notification_template_icon_bg=0x7f020059;
-        public static final int saturn=0x7f020055;
-        public static final int tile=0x7f020056;
-        public static final int uranus=0x7f020057;
-        public static final int venus=0x7f020058;
+        public static final int fam1=0x7f02004f;
+        public static final int fam2=0x7f020050;
+        public static final int fam3=0x7f020051;
+        public static final int fam4=0x7f020052;
+        public static final int fri1=0x7f020053;
+        public static final int fri2=0x7f020054;
+        public static final int fri3=0x7f020055;
+        public static final int fri4=0x7f020056;
+        public static final int fri5=0x7f020057;
+        public static final int fri6=0x7f020058;
+        public static final int ic_drawer=0x7f020059;
+        public static final int ic_launcher=0x7f02005a;
+        public static final int jupiter=0x7f02005b;
+        public static final int mars=0x7f02005c;
+        public static final int mercury=0x7f02005d;
+        public static final int neptune=0x7f02005e;
+        public static final int notification_template_icon_bg=0x7f020067;
+        public static final int saturn=0x7f02005f;
+        public static final int teac1=0x7f020060;
+        public static final int teac2=0x7f020061;
+        public static final int teac3=0x7f020062;
+        public static final int teac4=0x7f020063;
+        public static final int tile=0x7f020064;
+        public static final int uranus=0x7f020065;
+        public static final int venus=0x7f020066;
     }
     public static final class id {
         public static final int action0=0x7f0b0058;
@@ -2079,20 +2093,23 @@ containing a value of this type.
         public static final int activity_main=0x7f03001a;
         public static final int activity_navigation_drawer=0x7f03001b;
         public static final int drawer_list_item=0x7f03001c;
-        public static final int fragment_planet=0x7f03001d;
-        public static final int notification_media_action=0x7f03001e;
-        public static final int notification_media_cancel_action=0x7f03001f;
-        public static final int notification_template_big_media=0x7f030020;
-        public static final int notification_template_big_media_narrow=0x7f030021;
-        public static final int notification_template_lines=0x7f030022;
-        public static final int notification_template_media=0x7f030023;
-        public static final int notification_template_part_chronometer=0x7f030024;
-        public static final int notification_template_part_time=0x7f030025;
-        public static final int sample_dashboard_item=0x7f030026;
-        public static final int select_dialog_item_material=0x7f030027;
-        public static final int select_dialog_multichoice_material=0x7f030028;
-        public static final int select_dialog_singlechoice_material=0x7f030029;
-        public static final int support_simple_spinner_dropdown_item=0x7f03002a;
+        public static final int fragment_family=0x7f03001d;
+        public static final int fragment_friends=0x7f03001e;
+        public static final int fragment_planet=0x7f03001f;
+        public static final int fragment_teacher=0x7f030020;
+        public static final int notification_media_action=0x7f030021;
+        public static final int notification_media_cancel_action=0x7f030022;
+        public static final int notification_template_big_media=0x7f030023;
+        public static final int notification_template_big_media_narrow=0x7f030024;
+        public static final int notification_template_lines=0x7f030025;
+        public static final int notification_template_media=0x7f030026;
+        public static final int notification_template_part_chronometer=0x7f030027;
+        public static final int notification_template_part_time=0x7f030028;
+        public static final int sample_dashboard_item=0x7f030029;
+        public static final int select_dialog_item_material=0x7f03002a;
+        public static final int select_dialog_multichoice_material=0x7f03002b;
+        public static final int select_dialog_singlechoice_material=0x7f03002c;
+        public static final int support_simple_spinner_dropdown_item=0x7f03002d;
     }
     public static final class menu {
         public static final int navigation_drawer=0x7f0d0000;
@@ -2135,8 +2152,9 @@ containing a value of this type.
         public static final int drawer_close=0x7f050023;
         public static final int drawer_open=0x7f050024;
         public static final int intro_message=0x7f050025;
-        public static final int navigationdraweractivity_description=0x7f050026;
-        public static final int navigationdraweractivity_title=0x7f050027;
+        public static final int myname=0x7f050026;
+        public static final int navigationdraweractivity_description=0x7f050027;
+        public static final int navigationdraweractivity_title=0x7f050028;
         public static final int status_bar_notification_info_overflow=0x7f050013;
     }
     public static final class style {
--- Application/src/main/java/com/example/android/navigationdrawer/NavigationDrawerActivity.java
+++ Application/src/main/java/com/example/android/navigationdrawer/NavigationDrawerActivity.java
@@ -229,16 +229,15 @@ public class NavigationDrawerActivity extends Activity implements PlanetAdapter.
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_planet, container, false);
+            View rootView = null;
             int i = getArguments().getInt(ARG_PLANET_NUMBER);
-            String planet = getResources().getStringArray(R.array.planets_array)[i];
+            if(i==0)
+                rootView = inflater.inflate(R.layout.fragment_family, container, false);
+            else if(i==1)
+                rootView = inflater.inflate(R.layout.fragment_friends, container, false);
+            else if(i==2)
+                rootView = inflater.inflate(R.layout.fragment_teacher, container, false);
 
-            int imageId = getResources().getIdentifier(planet.toLowerCase(Locale.getDefault()),
-                    "drawable", getActivity().getPackageName());
-            ImageView iv = ((ImageView) rootView.findViewById(R.id.image));
-            iv.setImageResource(imageId);
-
-            getActivity().setTitle(planet);
             return rootView;
         }
     }
